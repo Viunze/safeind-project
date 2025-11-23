e30153862f1b93678ed09c6aab5b59387241be5f
Viunze <cellodestian5@gmail.com>
2025-11-23 11:53:14 +0700

    Update auth.js

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -1,9 +1,12 @@
 // src/lib/firebase/auth.js
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import { app } from './config'; // Asumsi path ke firebase/config Anda
-
-// Mendapatkan instance Auth
-export const auth = getAuth(app); // Ekspor instance auth
+import { 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  GoogleAuthProvider, 
+  signInWithPopup, 
+  signOut 
+} from 'firebase/auth';
+import { auth } from './config'; // Import instance auth
 
 // Fungsi Sign In
 export const handleSignIn = (email, password) => {
