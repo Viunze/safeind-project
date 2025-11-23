2754a691e14711cd1194d5b6c9dc14cec59618ea
Viunze <cellodestian5@gmail.com>
2025-11-23 10:10:50 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,13 @@
 // src/pages/_app.js
-import '../styles/globals.css';
-import { AuthProvider } from '../context/AuthContext'; // Import AuthProvider
+
+// PASTIKAN import path ini benar
+import { AuthProvider } from '../context/AuthContext'; 
 
 function MyApp({ Component, pageProps }) {
   return (
-    // Wrap the entire application with AuthProvider
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
-
-export default MyApp;
+// ...
