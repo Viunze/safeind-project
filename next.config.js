c203cec992bd052e0958d1cae867e41b09bc94e3
Viunze <cellodestian5@gmail.com>
2025-11-23 11:05:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', 
+  output: 'standalone',
+  images: {
+    // Tambahkan domain ini agar Next.js bisa memuat logo dari luar
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'files.catbox.moe',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
