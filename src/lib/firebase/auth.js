08ce4f9c9fb292f8ca228e076bf64abe40589b28
Viunze <cellodestian5@gmail.com>
2025-11-23 12:17:29 +0700

    Update auth.js

--- src/lib/firebase/auth.js
+++ src/lib/firebase/auth.js
@@ -6,25 +6,21 @@ import {
   signInWithPopup, 
   signOut 
 } from 'firebase/auth';
-import { auth } from './config'; // Mengimpor auth dari config.js
+import { auth } from './config'; 
 
-// Fungsi Sign In
 export const handleSignIn = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
 };
 
-// Fungsi Sign Up
 export const handleSignUp = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
 };
 
-// Fungsi Google Sign In
 export const handleGoogleSignIn = () => {
     const provider = new GoogleAuthProvider();
     return signInWithPopup(auth, provider);
 };
 
-// Fungsi Sign Out
 export const handleSignOut = () => {
     return signOut(auth);
 };
