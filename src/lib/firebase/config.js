1c904fa14301d6b33b7d85f0cec1b3c86de7bc3f
Viunze <cellodestian5@gmail.com>
2025-11-23 11:55:58 +0700

    Update config.js

--- src/lib/firebase/config.js
+++ src/lib/firebase/config.js
@@ -10,9 +10,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  // measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID, // Opsional
 };
 
 // Inisialisasi Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); // Ekspor instance Auth
+export const auth = getAuth(app); // EXPORT AUTH di sini
