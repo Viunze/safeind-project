f95c2a152f0ef5dcfde74da95b3e0f760e8146fa
Viunze <cellodestian5@gmail.com>
2025-11-23 11:40:41 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,14 @@
 // src/pages/_app.js
-import '../styles/globals.css'; 
+
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '../context/AuthContext'; 
-import { Toaster } from 'react-hot-toast'; // Import Toaster component
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
-      {/* Konfigurasi Toaster untuk notifikasi in-app */}
-      <Toaster 
-        position="top-right" 
-        toastOptions={{
-          // Custom styling menggunakan class Tailwind untuk tampilan dark mode/neon
-          className: 'bg-dark-background text-white shadow-xl border border-primary-neon/30',
-          style: {
-            background: '#1f2937', 
-            color: '#f3f4f6',
-          }
-        }}
-      />
+      <Toaster position="bottom-center" reverseOrder={false} />
     </AuthProvider>
   );
 }
